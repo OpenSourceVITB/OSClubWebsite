0736b646c18d0a6af28fa6c8fa76bdcb7ea64cda
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-15 01:20:34 +0530

    feat : added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <div className="absolute top-0 z-[-2] h-[90rem] w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="absolute top-0 z-[-2] h-[151rem] sm:h-[97rem] w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { EventCard } from "@/components/custom/event-card";
 import { Hero } from "@/components/custom/hero";
 import { Navbar } from "@/components/custom/navbar";
 import { events } from "@/lib/data/event-data";
+import { GitHubLogoIcon, LinkedInLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons"
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,7 +18,23 @@ export default function Home() {
             )
           })}
         </div>
+        <div className="w-full flex flex-col justify-center items-center my-16">
+          <h1 className="text-xl text-slate-400">The Open-Source Club</h1>
+          <div className="flex justify-between items-center gap-2">
+            <Link href={"https://www.linkedin.com/company/opensourcevitb/"} target="_blank">
+              <LinkedInLogoIcon className="text-purple-800" height={20} width={20} />
+            </Link>
+            <Link href={"https://github.com/OpenSourceVITB"} target="_blank">
+              <GitHubLogoIcon className="text-purple-800" height={20} width={20} />
+            </Link>
+            <Link href={"https://www.instagram.com/opensourcevitb"} target="_blank">
+              <InstagramLogoIcon className="text-purple-800" height={20} width={20} />
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
 }
+
+
