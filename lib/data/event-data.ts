1580907bfaaa253cb22db22ad32cff8d2682aeba
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-15 08:51:47 +0530

    fix: minor ui fixes

--- lib/data/event-data.ts
+++ lib/data/event-data.ts
@@ -1,34 +1,5 @@
 export const events = [
   {
-    name: "Inauguration & Tech Symposium",
-    description:
-      "Join us on 22nd February from 11AM to 1PM in room AB206 for an insightful lecture by a distinguished GSOC '23 contributor. He will offer valuable insights on initiating your journey into open source development, sharing tips and strategies from his GSOC experience. Additionally, participants will engage in a hands-on workshop focusing on the fundamentals of Git and GitHub, empowering them to kickstart their own open source projects effectively.",
-    faculty: "Dr. Preetam Suman",
-    studentCoordinator: "Vinayak Pandey",
-    studentCoordinatorNumber: "9555474840",
-    venue: "AB 206",
-    date: "22nd February",
-    time: "11:00 AM to 1:30 PM",
-    registrationCost: "Rs 69",
-    is_for_external_students: false,
-    clubName: "Open-Source Club",
-    formLinkVIT: "https://forms.gle/GLKhXAtEFKG9jjCd9",
-    formLinkExt: "",
-    coordinators: [
-      {
-        name: "Pankaj Khilar",
-        phone: "Vice President",
-        role: "Vice President",
-      },
-      {
-        name: "Harsh Shukla",
-        phone: "9344116227",
-        role: "Deputy Vice President",
-      },
-    ],
-    image_url: "/images/tech_symposium.png",
-  },
-  {
     name: "Open-Source Sprint",
     description:
       "Join our Open Source Sprint where students select from a range of repositories across different domains, each with incomplete code and bugs. Participants fork the chosen repository, fix the issues, and submit a pull request. They must include screenshots of the functioning solution and details of their commits in the pull request description. Don't miss this chance to showcase your skills and contribute to the open-source community!",
@@ -57,5 +28,34 @@ export const events = [
       },
     ],
     image_url: "/images/open-source-sprint.png",
+  },
+  {
+    name: "Inauguration & Tech Symposium",
+    description:
+      "Join us on 22nd February from 11AM to 1PM in room AB206 for an insightful lecture by a distinguished GSOC '23 contributor. He will offer valuable insights on initiating your journey into open source development, sharing tips and strategies from his GSOC experience. Additionally, participants will engage in a hands-on workshop focusing on the fundamentals of Git and GitHub, empowering them to kickstart their own open source projects effectively.",
+    faculty: "Dr. Preetam Suman",
+    studentCoordinator: "Vinayak Pandey",
+    studentCoordinatorNumber: "9555474840",
+    venue: "AB 206",
+    date: "22nd February",
+    time: "11:00 AM to 1:30 PM",
+    registrationCost: "Rs 69",
+    is_for_external_students: false,
+    clubName: "Open-Source Club",
+    formLinkVIT: "https://forms.gle/GLKhXAtEFKG9jjCd9",
+    formLinkExt: "",
+    coordinators: [
+      {
+        name: "Pankaj Khilar",
+        phone: "Vice President",
+        role: "Vice President",
+      },
+      {
+        name: "Harsh Shukla",
+        phone: "9344116227",
+        role: "Deputy Vice President",
+      },
+    ],
+    image_url: "/images/tech_symposium.png",
   },
 ];
